2efba7ffcd0f882a4b40b7b943235aeae7aedc56
yuanwenhao <actions.win@foxmail.com>
2025-03-26 10:20:46 +0800

    修复打包卡住的问题

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,31 @@
 import type { DeployOptions } from './types'
-import { createWriteStream } from 'node:fs'
-import { unlink } from 'node:fs/promises'
+import { createWriteStream, existsSync } from 'node:fs'
+import { mkdir, unlink } from 'node:fs/promises'
 import path from 'node:path'
 import archiver from 'archiver'
 import consola from 'consola'
 import { NodeSSH } from 'node-ssh'
 import { configLoader } from './config'
 
+// `${projectRoot}/node_modules/.cache/light-deploy`
+let cacheDeployDir: string
+
 async function zipFiles(config: DeployOptions) {
   const { zipFilename, distDir } = config.project
 
+  consola.info(`${zipFilename} 打包中...`)
+  await _deleteLocalZipFile(config)
+  await makeDir()
+
   return new Promise<void>((resolve, reject) => {
-    consola.info(`${zipFilename} 打包中...`)
     const archive = archiver('zip', {
       zlib: { level: 9 },
     }).on('error', (err) => {
-      throw err
+      reject(err)
+      process.exit(1)
     })
 
-    const outputFilePath = path.resolve(distDir, zipFilename!)
-
+    const outputFilePath = path.resolve(cacheDeployDir, zipFilename!)
     const output = createWriteStream(outputFilePath)
 
     output.on('error', (err) => {
@@ -31,14 +37,24 @@ async function zipFiles(config: DeployOptions) {
     })
 
     output.on('close', () => {
-      consola.success(`${zipFilename} 打包成功, ${archive.pointer()} bytes`)
+      consola.success(`output-close--${zipFilename} 打包成功, ${archive.pointer()} bytes`)
       resolve()
+    })
+
+    archive.on('warning', (err) => {
+      console.warn(err)
     })
 
     archive.pipe(output)
     archive.directory(distDir, false)
     archive.finalize()
   })
+}
+
+async function makeDir() {
+  if (!existsSync(cacheDeployDir)) {
+    await mkdir(cacheDeployDir, { recursive: true })
+  }
 }
 
 async function createSSh(config: DeployOptions) {
@@ -57,12 +73,12 @@ async function createSSh(config: DeployOptions) {
 }
 
 async function uploadFile(ssh: NodeSSH, config: DeployOptions) {
-  const { distDir, serverWebDir, zipFilename } = config.project
+  const { serverWebDir, zipFilename } = config.project
 
   consola.info(`上传 ${zipFilename!} 至目录 ${config.project.serverWebDir}`)
 
   try {
-    await ssh.putFile(path.resolve(distDir, zipFilename!), path.join(serverWebDir, zipFilename!))
+    await ssh.putFile(path.resolve(cacheDeployDir, zipFilename!), path.join(serverWebDir, zipFilename!))
     consola.success('上传成功')
   }
   catch (error) {
@@ -85,11 +101,21 @@ async function unzipFile(ssh: NodeSSH, config: DeployOptions) {
   }
 }
 
+async function _deleteLocalZipFile(config: DeployOptions) {
+  const { zipFilename } = config.project
+  const zipFilePath = path.resolve(cacheDeployDir, zipFilename!)
+
+  const isExists = existsSync(zipFilePath)
+  if (!isExists)
+    return
+
+  await unlink(zipFilePath)
+}
+
 async function deleteLocalZipFile(config: DeployOptions) {
-  const { distDir, zipFilename } = config.project
-  const zipFilePath = path.resolve(distDir, zipFilename!)
+  const { zipFilename } = config.project
   try {
-    await unlink(zipFilePath)
+    await _deleteLocalZipFile(config)
     consola.success(`${zipFilename} 本地删除成功`)
   }
   catch (err) {
@@ -104,6 +130,7 @@ async function main() {
 
   const projectRoot = path.resolve(loader.sources[0], '../')
   const distDir = path.resolve(projectRoot, config.project.distDir)
+  cacheDeployDir = path.resolve(projectRoot, 'node_modules/.cache/light-deploy')
 
   config.project.distDir = distDir
   config.project.zipFilename ||= 'deploy_dist.zip'
